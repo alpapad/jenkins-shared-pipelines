dbe2c9dfeac00a17b419d5ba4cb9f04389493ac8
alpapad <alpapad@gmail.com>
2022-09-15 10:16:12 +0300

    Create buildPipeline.groovy

---
+++ vars/buildPipeline.groovy
@@ -0,0 +1,51 @@
+def call(Closure body) {
+    
+    def pipelineParams= [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
+
+    pipeline {
+        agent { label "linux" }
+        stages {
+            stage("echo parameters") {
+                steps {
+                    sh "env | sort"
+                }
+            }
+            stage("Prepare Build Environment") {
+                steps {
+                    prepareBuildEnvironment()
+                    echo "Prepare Build Environment ${pipelineParams.email}"
+                }
+            }
+            stage("Source Code Checkout") {
+                steps {
+                    echo 'Source Code Checkout'
+                    sh "ls"
+                }
+            }
+            stage("Build Application") {
+                steps {
+                    echo 'build'
+                }
+            }
+            stage("Publish Artifacts") {
+                steps {
+                    echo 'Publish Artifacts'
+                }
+            }
+            stage("Deploy Application") {
+                steps {
+                     echo 'Deploy Application'
+                }
+            }
+  
+        }
+        post {
+            always {
+              echo 'Post action'
+            }
+        }
+    }
+}
